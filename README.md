4d180ee70e22d3b72e31ce0ea5ad0ac01ed611c3
VMan-2002 <vman.2002.au@gmail.com>
2025-11-18 13:19:51 +1100

    beebi

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # ElementCatlatro
+### Requires [TopUpLib](https://github.com/VMan-2002/TopUpLib)
+
 element cattos balatro mod (REAL)
---
+++ lovely/pronounbadges.lua
@@ -0,0 +1,5 @@
+AAAAAAAAAAAAAAAAAAAAAAA
+	if badge.key == "ecatto_eaglenoise_any" then
+		badges[#badges].nodes[1].config.pixellated_rect_texture = elementcattos.usa_flag
+	end
+end
